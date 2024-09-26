ca1ae395507e32325dfe2d8f2619f8cb767450b9
nielsgreniman <105637299+nielsgreniman@users.noreply.github.com>
2024-09-26 22:57:21 +0200

    added new emojis + shuffle effect

--- MatchGame/MainWindow.xaml.cs
+++ MatchGame/MainWindow.xaml.cs
@@ -24,10 +24,11 @@ namespace MatchGame
         DispatcherTimer timer = new DispatcherTimer();
         int tenthsOfSecondElapsed;
         int matchesFound;
+        float bestTime = float.MaxValue;
+
         public MainWindow()
         {
             InitializeComponent();
-
             timer.Interval = TimeSpan.FromSeconds(.1);
             timer.Tick += Timer_Tick;
 
@@ -36,45 +37,90 @@ namespace MatchGame
 
         private void Timer_Tick(object sender, EventArgs e)
         {
-            tenthsOfSecondElapsed++;
-            timeTextBlock.Text = (tenthsOfSecondElapsed / 10F).ToString("0.0s");
-            if (matchesFound == 8)
+            tenthsOfSecondElapsed--;
+            timeElapsedTextBlock.Text = (tenthsOfSecondElapsed / 10F).ToString("0.0s");
+            if (matchesFound == 8) 
             {
                 timer.Stop();
-                timeTextBlock.Text = timeTextBlock.Text + " - Play again?";
+                float currentTime = (300/10F - tenthsOfSecondElapsed/10F);
+                
+
+                if (currentTime < bestTime)
+                {
+                    bestTime = currentTime;
+                    bestTimeTextBlock.Text = "Best time: " + bestTime;
+                    timeElapsedTextBlock.Text = "New best time: " + bestTime;
+                }
+                else
+                {
+                    timeElapsedTextBlock.Text = currentTime + " Well done! Play again?";
+                }
+                                    
+            }
+            else if (tenthsOfSecondElapsed == 0)
+            {
+                timer.Stop();
+                timeElapsedTextBlock.Text = "Time's up! Play again?";
             }
         }
 
         private void SetUpGame()
         {
-            List<string> animalEmoji = new List<string>()
+            List<string> animalEmojis = new List<string>()
             {
-                "🐒", "🐒",
-                "🦍", "🦍",
-                "🐕", "🐕",
-                "🐅", "🐅",
-                "🐈", "🐈",
-                "🐍", "🐍",
-                "🧸", "🧸",
-                "🦁", "🦁",
+                "🐒",//"🐒",
+                "🦍",//"🦍",
+                "🐕",//"🐕",
+                "🐅",//"🐅",
+                "🐈",//"🐈",
+                "🐍",//"🐍",
+                "🧸",//"🧸",
+                "🦁",//"🦁",
+                "🦚",//"🦚",
+                "🦉",//"🦉",
+                "🦆",//"🦆",
+                "🕷️",//"🕷️",
+                "🐢",//"🐢",
+                "🐭",//"🐭",
+                "🐉",//"🐉",
             };
 
-            Random random = new Random();   
+            Random random = new Random();
+            List<string> selectedEmojis = new List<string>();
+
+            for (int i = 0; i < 8; i++) // selects 8 emojis from the animalEmojis List
+            { 
+                int index = random.Next(animalEmojis.Count);
+                string emoji = animalEmojis[index];
+                selectedEmojis.Add(emoji);
+                animalEmojis.RemoveAt(index);
+            }
+
+            // Duplicate emojis to create pairs
+            List<string> animalEmojiPairs = new List<string>(); 
+            foreach (var emoji in selectedEmojis)
+            {
+                animalEmojiPairs.Add(emoji);
+                animalEmojiPairs.Add(emoji);
+            }
+
+            // Shuffle the emoji pairs
+            List<string> shuffledAnimalEmojis = animalEmojiPairs.OrderBy(a => random.Next()).ToList();
 
             foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>()) 
             {
-                if (textBlock.Name != "timeTextBlock")
+                if (textBlock.Name != "timeElapsedTextBlock" && textBlock.Name != "bestTimeTextBlock")
                 {
                     textBlock.Visibility = Visibility.Visible;
-                    int index = random.Next(animalEmoji.Count);
-                    string nextEmoji = animalEmoji[index];
+                    int index = random.Next(shuffledAnimalEmojis.Count);
+                    string nextEmoji = shuffledAnimalEmojis[index];
                     textBlock.Text = nextEmoji;
-                    animalEmoji.RemoveAt(index);
+                    shuffledAnimalEmojis.RemoveAt(index);
                 }
             }
 
             timer.Start();
-            tenthsOfSecondElapsed = 0;
+            tenthsOfSecondElapsed = 300;
             matchesFound = 0;
         }
 
@@ -106,7 +152,7 @@ namespace MatchGame
 
         private void TimeTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            if (matchesFound == 8)
+            if (matchesFound == 8 || tenthsOfSecondElapsed == 0)
             {
                 SetUpGame();
             }
